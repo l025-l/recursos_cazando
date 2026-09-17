b1efb5b5cef9eecdd7ae0a75eb2821c2806796bb
l025-l <cristianpegatodo22@gmail.com>
2026-09-16 22:46:43 -0500

    difinir e modificar variables y constantes

--- cazando.js
+++ cazando.js
@@ -1,13 +1,27 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
 
-const recAncho= 200;
-const recAlto= 100;
+const ALTO_GATO=100
+const ANCHO_GATO=200
+const ALTO_COMIDA=40
+const ANCHO_COMIDA=60
 
-const x= (canvas.width/2)-(recAncho/2);
-const y= (canvas.height/2)-(recAlto/2);
+let gatoX=(canvas.width/2)-(ANCHO_GATO/2)
+let gatoY=(canvas.height/2)-(ALTO_GATO/2)
+let comidaX=0
+let comidaY=0
+
+function iniciarJuego(){
+    graficarGato();
+    graficarComida();
+}
 
 function graficarGato(){
     ctx.fillStyle="blue";
-    ctx.fillRect(x,y,recAncho,recAlto);
+    ctx.fillRect(gatoX,gatoY,ANCHO_GATO,ALTO_GATO);
+}
+
+function graficarComida(){
+    ctx.fillStyle="red";
+    ctx.fillRect(comidaX,comidaY,ANCHO_COMIDA,ALTO_COMIDA);
 }
