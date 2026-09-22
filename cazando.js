132f184c777751ae77ccbbf40faf4a733e89446a
l025-l <cristianpegatodo22@gmail.com>
2026-09-21 20:28:09 -0500

    mover gato hacia la izquierda

--- cazando.js
+++ cazando.js
@@ -34,3 +34,16 @@ function graficarRectangulo(x,y,ancho,alto,color){
     ctx.fillStyle=color;
     ctx.fillRect(x,y,ancho,alto);
 }
+
+function limpiarCanva(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+function moverIzquierda(){
+    gatoX=gatoX-10;
+
+    limpiarCanva();
+    graficarGato();
+    graficarComida();
+
+}
